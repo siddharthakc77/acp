6c252a234019846f10dbeb2b1e66912e021b9105
siddharthakc77 <siddharthakc77@gmail.com>
2026-03-15 21:51:54 +0545

    Database connected

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,29 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
-
 app.use(express.json());
 
-// route
-app.get("/", (req, res) => {
-  res.send("Hello");
+// connect MongoDB
+mongoose.connect("mongodb://localhost:27017/library")
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
+
+// test
+const Book = mongoose.model("Book", {
+  title: String,
+  author: String
+});
+
+// insert test book
+app.get("/add", async (req, res) => {
+  const book = new Book({
+    title: "Test Book",
+    author: "Admin"
+  });
+
+  await book.save();
+  res.send("Book inserted");
 });
 
 app.listen(5000, () => {console.log("http://localhost:5000");});
